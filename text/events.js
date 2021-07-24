d0b8eacc3c2ae7bcd32c9fe12c424f21970a9e23
camog4224 <camog4224@gmail.com>
2021-07-24 00:10:37 -0400

    added ideas and events

---
+++ text/events.js
@@ -0,0 +1,23 @@
+/* different people coming to the town will come at different stages
+like the
+*/
+events = {
+  "single": [
+    "Carpenter",
+    "Farmer",
+    "Artisan",
+    "Merchant",
+    "Chief",
+    "Assassin",
+    "Courier",
+    "Guard",
+    "Priest",
+    "Archeologist",
+    "Weavers",
+  ],
+  "group" : [
+
+  ]
+
+
+}
