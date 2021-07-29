cd3dfa0ce60d0fc8f7816aba8b81c4eac7dfa6e4
camog4224 <camog4224@gmail.com>
2021-07-28 21:55:33 -0400

    resources display and remove correctly

--- scripts/home.js
+++ scripts/home.js
@@ -3,22 +3,80 @@
 
 setTimeout(turnOnPopup, 2000);
 
+// setInterval(checkResources,100);
+
 var leftSide  = document.querySelector("#left");
 var middleSide = document.querySelector("#middle");
 var rightSide = document.querySelector("#right");
 var popup = document.querySelector("#popup");
 
+var startFire = document.querySelector("#startFire");
+var stokeFire = document.querySelector("#stokeFire");
+
+var woodPile = document.querySelector("#wood");
+
+startFire.addEventListener('click', startFireFunc);
+stokeFire.addEventListener('click', stokeFireFunc);
+
+var onFire = false;
+
+var resourceDict = {
+  "wood" : 5,
+  "charcoal" : 5,
+  "stone" : 5
+};
+
+function updateResources(){
+  // for(var i = 0; i < table.rows.length; i++){
+  //
+  // }
+  // var resD = Object.keys(resourceDict);
+  var index = 0;
+  for(var key in resourceDict){
+    console.log(key);
+    table.rows[index].cells[1].innerHTML = resourceDict[key];
+    // var value = resourceDict[temp];
+    index++;
+  }
+}
+
+function startFireFunc(){
+  // alert("FIRE STARTED");
+  if(onFire == false && resourceDict.wood > 0){
+    resourceDict.wood--;
+    updateResources();
+    onFire = true;
+  }
+}
+
+function stokeFireFunc(){
+  // alert("FIRE STOKED");
+  if(onFire == true && resourceDict.wood > 0){
+    resourceDict.wood--;
+
+    updateResources();
+  }
+}
+
+
+
 var table = document.createElement("TABLE");
-table.setAttribute("id","table");
-for(var i = 0; i < 5; i++){
-  addRow();
+table.setAttribute("class","border");
+var res = Object.keys(resourceDict);
+// console.log(res[0]);
+// console.log(resourceDict["wood"]);
+for(var i = 0; i < res.length; i++){
+  // console.log(res[i], resourceDict.res[i]);
+  addRow(res[i], resourceDict[res[i]]);
 }
 
 function turnOnPopup(){
   popup.classList.add('poppedUp');
 }
 
-// console.log(superInfo.Levels);
+// console.log(resourceDict);
+// console.log(resourceDict.length);
+// console.log(Object.keys(resourceDict).length);
 // console.log(JSON.stringify(superInfo.Levels));
 // console.log(superInfo.Levels[0]);
 // console.log(Object.keys(superInfo.Levels[0])[0]);
@@ -31,12 +89,13 @@ for(var i = 0; i < infoLen; i++){
 }
 
 
-function addRow(){
-  var row = table.insertRow(0);
+function addRow(name, val){
+  console.log(table.rows.length);
+  var row = table.insertRow(-1);
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
-  cell1.innerHTML = "NAME";
-  cell2.innerHTML = "VALUE";
+  cell1.innerHTML = name;
+  cell2.innerHTML = val;
 }
 rightSide.appendChild(table);
 
